18a108902c1f26dd921ddbed23a3d058de294c2b
Maxime REBIBO <maximerebibo@gmail.com>
2016-11-26 15:02:28 +0100

    Reprise du projet

---
+++ NotesPerso/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NotesPerso
+//
+//  Created by Maxime REBIBO on 24/11/2016.
+//  Copyright © 2016 Maxime. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var Notes: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
